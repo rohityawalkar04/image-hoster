b0a6f2ef04537fb44dd712df23df8854c68892d2
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2021-09-26 14:48:58 +0530

    [Rohit] Displays error when user tries to edit a image which he/she has not created

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -92,13 +93,21 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, Model model, HttpSession session, RedirectAttributes redirectAttributes) {
         Image image = imageService.getImage(imageId);
-
+        if(!isImgOwnerLoggedIn(image, session)){
+            redirectAttributes.addFlashAttribute("editError", true);
+            return "redirect:/images/" + image.getId() + '/' + image.getTitle();
+        }
         String tags = convertTagsToString(image.getTags());
         model.addAttribute("image", image);
         model.addAttribute("tags", tags);
         return "images/edit";
+    }
+
+    private boolean isImgOwnerLoggedIn(Image image, HttpSession session){
+        User user = (User) session.getAttribute("loggeduser");
+        return Objects.equals(user.getId(), image.getUser().getId());
     }
 
     //This controller method is called when the request pattern is of type 'images/edit' and also the incoming request is of PUT type
